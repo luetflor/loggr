7401309adb810156e77cae854ba7320a0ee8af08
Monotrox99 <florianl.xd@gmail.com>
2020-08-14 19:13:39 +0200

    Added a bottom Row to allow to add new Values

--- lib/DataViewingWidgets/GridDataView.dart
+++ lib/DataViewingWidgets/GridDataView.dart
@@ -32,16 +32,35 @@ class GridDataState extends State<GridDataView>
               (context, index) {
                 int col = (index/page.sets.length).floor()-1;
                 int row = index%page.sets.length;
-                if(col == -1)
-                  return Center(child: GestureDetector(
+                //Title Bar
+                if(col == -1) return Center(child: GestureDetector(
                     child: Text(page.sets[row].name, style: style),
                     onLongPress: () => showSetModal(context, page.sets[row], data, page),
                   ),);
+                //Extra Bottom Row to add Values
+                if(col == page.sets[0].values.length) return Center(child: TextField(
+                  decoration: InputDecoration.collapsed(hintText: '---'),
+                  style: style,
+                  keyboardType: TextInputType.numberWithOptions(signed: true, decimal: true),
+                  textAlign: TextAlign.center,
+                  onSubmitted: (text) {
+                    double val = double.tryParse(text);
+                    //Fill every Set with null to ensure same-length lists
+                    if(val != null) {
+                      for(var set in page.sets) {
+                        if(set == page.sets[row]) set.addValue(val);
+                        else set.addValue(null);
+                      }
+                    }
+                  },
+                ),);
+                //Show actual Values
                 double val = page.sets[row].values[col];
                 String text = val == null ? '' : val.toString();
                 return Center(child: TextField(
                   decoration: InputDecoration.collapsed(hintText: '---'),
                   style: style,
+                  keyboardType: TextInputType.numberWithOptions(signed: true, decimal: true),
                   controller: TextEditingController(text: text),
                   textAlign: TextAlign.center,
                   onSubmitted: (text) {
@@ -50,7 +69,7 @@ class GridDataState extends State<GridDataView>
                   },
                 ),);
               },
-              childCount: page.sets.length*(page.sets[0].values.length+1)
+              childCount: page.sets.length*(page.sets[0].values.length+2)
           ),
         );
         return SliverList(delegate: SliverChildListDelegate([
