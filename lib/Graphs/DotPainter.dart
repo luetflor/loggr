9df86dde169748c55bb62e6b32dc0038168b3a53
Monotrox99 <florianl.xd@gmail.com>
2020-08-24 20:40:10 +0200

    Added Bound Check for DotPainter.dart and fadeout of Points

--- lib/Graphs/DotPainter.dart
+++ lib/Graphs/DotPainter.dart
@@ -5,6 +5,8 @@ import 'package:loggr/Data/LoggrPage.dart';
 
 class DotPainter extends CustomPainter
 {
+  final BLUR_OUT = 15.0;
+
   double minX, minY;
   double extentX, extentY;
 
@@ -20,11 +22,20 @@ class DotPainter extends CustomPainter
       ..color = data.accent
       ..style = PaintingStyle.fill;
 
+    Paint fade = Paint()
+      ..color = data.accent
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 2.0;
+
     for(DataSet set in output) {
       for(int i=0; i<set.values.length; i++) {
         if(input.values[i] != null && set.values[i] != null) {
           Offset pos = Offset(toScreenX(input.values[i], size), toScreenY(set.values[i], size));
-          canvas.drawCircle(pos, 5.0, pencil);
+          //Check for bounds
+          if(pos.dx >= BLUR_OUT && pos.dy >= BLUR_OUT && pos.dx <= size.width-BLUR_OUT && pos.dy <= size.height-BLUR_OUT)
+            canvas.drawCircle(pos, 5.0, pencil);
+          else if(pos.dx >= 0 && pos.dy >= 0 && pos.dx <= size.width && pos.dy <= size.height)
+            canvas.drawCircle(pos, 4.0, fade);
         }
       }
     }
